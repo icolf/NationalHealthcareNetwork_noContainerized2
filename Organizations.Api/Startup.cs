b2d471b5bc0f0c85624de2cdaff4af46a682187b
Luis E. Figueroa <icolfigueroa@gmail.com>
2019-07-18 10:22:04 -0500

    Adding Global Exception Handling

--- Organizations.Api/Startup.cs
+++ Organizations.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Formatters;
@@ -90,6 +91,14 @@ namespace Organizations.Api
             }
             else
             {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("An unexpected fault happened.  Try again later!");
+                    });
+                });
                 app.UseHsts();
             }
 
